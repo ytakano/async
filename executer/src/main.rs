cb6c8ae8910675a88272f997dc3a6fb17a73f766
Yuuki Takano <ytakanoster@gmail.com>
2021-01-02 17:18:57 +0900

    added executer

---
+++ executer/src/main.rs
@@ -0,0 +1,100 @@
+use futures::future::{BoxFuture, FutureExt};
+use futures::task::{waker_ref, ArcWake};
+use std::future::Future;
+use std::pin::Pin;
+use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
+use std::sync::{Arc, Mutex};
+use std::task::{Context, Poll};
+
+struct Hello {
+    state: StateHello,
+}
+
+enum StateHello {
+    HELLO,
+    WORLD,
+    END,
+}
+
+impl Hello {
+    fn new() -> Hello {
+        Hello {
+            state: StateHello::HELLO,
+        }
+    }
+}
+
+impl Future for Hello {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
+        match (*self).state {
+            StateHello::HELLO => {
+                print!("Hello, ");
+                (*self).state = StateHello::WORLD;
+                cx.waker().wake_by_ref();
+                return Poll::Pending;
+            }
+            StateHello::WORLD => {
+                println!("World!");
+                (*self).state = StateHello::END;
+                cx.waker().wake_by_ref();
+                return Poll::Pending;
+            }
+            StateHello::END => {
+                return Poll::Ready(());
+            }
+        }
+    }
+}
+
+struct Task {
+    future: Mutex<BoxFuture<'static, ()>>,
+    sender: SyncSender<Arc<Task>>,
+}
+
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        let self0 = arc_self.clone();
+        arc_self.sender.send(self0).unwrap();
+    }
+}
+
+struct Executer {
+    sender: SyncSender<Arc<Task>>,
+    receiver: Receiver<Arc<Task>>,
+}
+
+impl Executer {
+    fn new() -> Executer {
+        let (sender, receiver) = sync_channel(1024);
+        Executer {
+            sender: sender,
+            receiver: receiver,
+        }
+    }
+
+    fn spawn(&self, future: impl Future<Output = ()> + 'static + Send) {
+        let future = future.boxed();
+        let task = Arc::new(Task {
+            future: Mutex::new(future),
+            sender: self.sender.clone(),
+        });
+        self.sender.send(task).unwrap();
+    }
+
+    fn run(&self) {
+        while let Ok(task) = self.receiver.recv() {
+            let mut future = task.future.lock().unwrap();
+            let waker = waker_ref(&task);
+            let mut ctx = Context::from_waker(&waker);
+            if future.as_mut().poll(&mut ctx) == Poll::Pending {};
+        }
+    }
+}
+
+fn main() {
+    let executer = Executer::new();
+    executer.spawn(Hello::new());
+    executer.run();
+}
